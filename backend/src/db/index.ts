9a21f8faca19f99dcf1805be47f6d2276245c6c4
rei-x <wszyzdgo@gmail.com>
2021-07-28 16:26:43 +0200

    Update mongodb-memory-server

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -3,9 +3,9 @@ import mongoose from 'mongoose';
 import paginationOptions from './paginationOptions';
 import getOffsetFromPage from './getOffsetFromPage';
 
-export const connectToDatabase = async (url: string) => mongoose.connect(url,
-  { useNewUrlParser: true, useUnifiedTopology: true });
+export const connectToDatabase = async (url: string) => mongoose.connect(url);
 
-export const closeConnectionToDatabase = async () => mongoose.connection.close();
+export const closeConnectionToDatabase = async () =>
+  mongoose.connection.close();
 
 export { paginationOptions, getOffsetFromPage };
--- backend/tests/utils/index.ts
+++ backend/tests/utils/index.ts
@@ -9,24 +9,26 @@ import config from '../../src/config';
 
 export const connectToMemoryDatabase = async () => {
   const mongod = new MongoMemoryServer();
-  const url = await mongod.getUri();
+  await mongod.start();
+  const url = mongod.getUri();
   await connectToDatabase(url);
 };
 
-interface TestUserInterface{
-  username?: string,
-  firstName?: string,
-  lastName?: string,
-  role?: 'user' | 'admin',
-  password?: string
+interface TestUserInterface {
+  username?: string;
+  firstName?: string;
+  lastName?: string;
+  role?: 'user' | 'admin';
+  password?: string;
 }
 
 export const createTestUser = async (testUser?: TestUserInterface) => {
   let passwordHash;
   const password = testUser?.password;
 
-  if (password && testUser?.role === 'user') throw new Error('Only admins can log in using password');
-  if (password) passwordHash = await bcrypt.hash((password as string), 5);
+  if (password && testUser?.role === 'user')
+    throw new Error('Only admins can log in using password');
+  if (password) passwordHash = await bcrypt.hash(password as string, 5);
 
   const userData = {
     username: 'johnwalles',
@@ -41,11 +43,16 @@ export const createTestUser = async (testUser?: TestUserInterface) => {
   return UserModel.create(userData);
 };
 
-export const getUserJWT = async (user: UserInterface): Promise<string> => new Promise((resolve) => {
-  jwt.sign(<JWTInterface>{
-    userId: user.id,
-    role: user.role,
-  }, config.SECRET, (_err, encoded) => {
-    resolve(`Bearer ${encoded}`);
+export const getUserJWT = async (user: UserInterface): Promise<string> =>
+  new Promise((resolve) => {
+    jwt.sign(
+      <JWTInterface>{
+        userId: user.id,
+        role: user.role,
+      },
+      config.SECRET,
+      (_err, encoded) => {
+        resolve(`Bearer ${encoded}`);
+      }
+    );
   });
-});
